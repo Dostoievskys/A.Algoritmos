b6bfc10b8386afc8e4cd036c5c3ff5f1184eb2ce
Jennifer <jennifer.portinof@utem.cl>
2020-04-24 17:44:32 -0400

    Plantilla

--- main.cpp
+++ main.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
+#include <cstdlib>
+
 
 using namespace std;
 
-int main()
-{
-    //Esto es un comentario
+/**
+ * Función que muestra los participantes del grupo
+ */
+void participantes();
 
-    cout << "Hello world!" << endl;
-    return 0;
+/**
+ * Taller computacional
+ * @param argc cantidad de argumentos
+ * @param argv argumentos
+ * @return El código de salida del programa
+ */
+int main(int argc, char** argv) {
+
+    /**
+     * Incluir acá la lógica del programa
+     *
+     */
+    if (argc > 1) {
+        // @TODO programar proyecto
+    } else {
+        // Mostrar los integrantes
+        participantes();
+    }
+    return EXIT_SUCCESS;
+}
+
+void participantes() {
+    std::cout << std::endl << "=== Taller 01 ===" << std::endl;
+    std::cout << std::endl << "Juanito"; // Reemplazar por su nombre
+    std::cout << std::endl << "Pedrito"; // Reemplazar por su nombre
+    std::cout << std::endl << "Dieguito" << std::endl; // Reemplazar por su nombre
 }
