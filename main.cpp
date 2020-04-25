63c5bfa6a8ef71213adf4fc9ede0c9615c072084
Braulio Argandoña <braulio.argandonac@utem.cl>
2020-04-25 01:31:01 -0400

    Funcion Idempotencia [CORREGIR]

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
 
 using namespace std;
@@ -12,19 +13,72 @@ void participantes();
  * @param argc cantidad de argumentos
  * @param argv argumentos
  * @return El código de salida del programa
+    1 Braulio
+    2 Jennifer
+    3 Fanny
+    4 Braulio
+    5 Jennifer
+    6 Fanny
+    7 Braulio
+    8 Jennifer
+    9 Fanny
  */
 int main(int argc, char** argv) {
 
     /**
      * Incluir acá la lógica del programa
-     * lala
+     *
      */
-    if (argc > 1) {
+    string lala="( ~ p || ~ p ) ( ~ p || ~ p ) ( ~ p || ~ p ) ( ~ p || ~ p ) ( ~ p || ~ p ) ( ~ p || ~ p )";
+    cout << lala <<endl;
+    int largo=lala.length();
+    cout<<largo<<endl;
+    if (argc > 0) { //Era mayor que 1
         // @TODO programar proyecto
+        for(int i=0; i<lala.length(); i++){//el largo es argv.lenght()
+              int c, t=0;
+              char aux[lala.length()];
+              int a=112;
+              if(lala[i]==a){
+                c=i+2;
+                if(lala[c]=='&'|| lala[c]=='|'){
+                        c+=3;
+                    if(lala[c]=='p'){
+                        int t=0;
+                        for(t;t<=i;t++){
+                            aux[t]=lala[t];
+                        }
+                        for(int u=c+1; u<lala.length();u++){
+                            aux[t]=lala[u];
+                            t++;
+                        }
+                        lala=aux;
+                        i=i+7;
+                        }
+
+                    if(lala[i-2]=='~'&& lala[c] =='~'){
+                            c+=2;
+                            if(lala[c]=='p'){
+                            for(t;t<=i;t++){
+                                aux[t]=lala[t];
+                            }
+                            for(int u=c+1; u<lala.length();u++){
+                                aux[t]=lala[u];
+                                t++;
+                            }
+                            lala=aux;
+                            i=i+7;
+                            }
+                        }
+                    }
+                }
+              }
     } else {
         // Mostrar los integrantes
         participantes();
     }
+    cout << lala <<endl;
+    system("Pause");
     return EXIT_SUCCESS;
 }
 
@@ -34,3 +88,4 @@ void participantes() {
     std::cout << std::endl << "Fanny Rivero"; // Reemplazar por su nombre
     std::cout << std::endl << "Jennifer Portiño" << std::endl; // Reemplazar por su nombre
 }
+
