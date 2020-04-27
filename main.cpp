f1b1ce14ed28ee65cba1a9189a04a6e116280851
Braulio Argandoña <braulio.argandonac@utem.cl>
2020-04-26 20:37:46 -0400

    Funcion Idempotencia recibe p, q o r segun lo necesitado

--- main.cpp
+++ main.cpp
@@ -1,91 +1,99 @@
 #include <iostream>
 #include <cstdlib>
-#include <string>
-
-
-using namespace std;
+#include<stdlib.h>
+#include<stdio.h>
+#include <string.h>
+#include <vector>
 
 //Función que muestra los participantes del grupo
 void participantes();
+void Idempotencia(std::vector<std::string>&, std::string);     //Funcion Idempotencia simplifica p,q o r -> a = p,q o r; hay asignar el valor
 
 /**
  * Taller computacional
  * @param argc cantidad de argumentos
  * @param argv argumentos
  * @return El código de salida del programa
-    1 Braulio
-    2 Jennifer
-    3 Fanny
-    4 Braulio
-    5 Jennifer
-    6 Fanny
-    7 Braulio
-    8 Jennifer
-    9 Fanny
- */
+*/
+
 int main(int argc, char** argv) {
-
-    /**
-     * Incluir acá la lógica del programa
-     *
-     */
-    string lala="( ~ p || ~ p ) ( ~ p || ~ p ) ( ~ p || ~ p ) ( ~ p || ~ p ) ( ~ p || ~ p ) ( ~ p || ~ p )";
-    cout << lala <<endl;
-    int largo=lala.length();
-    cout<<largo<<endl;
-    if (argc > 0) { //Era mayor que 1
-        // @TODO programar proyecto
-        for(int i=0; i<lala.length(); i++){//el largo es argv.lenght()
-              int c, t=0;
-              char aux[lala.length()];
-              int a=112;
-              if(lala[i]==a){
-                c=i+2;
-                if(lala[c]=='&'|| lala[c]=='|'){
-                        c+=3;
-                    if(lala[c]=='p'){
-                        int t=0;
-                        for(t;t<=i;t++){
-                            aux[t]=lala[t];
-                        }
-                        for(int u=c+1; u<lala.length();u++){
-                            aux[t]=lala[u];
-                            t++;
-                        }
-                        lala=aux;
-                        i=i+7;
-                        }
-
-                    if(lala[i-2]=='~'&& lala[c] =='~'){
-                            c+=2;
-                            if(lala[c]=='p'){
-                            for(t;t<=i;t++){
-                                aux[t]=lala[t];
-                            }
-                            for(int u=c+1; u<lala.length();u++){
-                                aux[t]=lala[u];
-                                t++;
-                            }
-                            lala=aux;
-                            i=i+7;
-                            }
-                        }
-                    }
-                }
-              }
-    } else {
+    std::vector<std::string> argList;  // Aqui se almacena lo de argv y aqui voy modificando
+    std::string a="p";                 // Variable para comparar en idempotencia, fue de ejemplo
+    if (argc > 1) {
+        for(int i=1;i<argc;i++){
+            argList.push_back(argv[i]);     //Agrego uno por uno lo recibido por consola ignorando el argv[0] que es el nombre del programa
+        }
+    }
+    else {
         // Mostrar los integrantes
         participantes();
     }
-    cout << lala <<endl;
+    std::cout << std::endl << "=== Original ===" << std::endl;  //Imprimo lo ingresado inicialmente
+    for(int i=0;i<argList.size();i++){
+        std::cout<<argList[i]<<" Posicion: "<<i<<std::endl;
+    }
+    Idempotencia(argList, a);                                    //Maravillosa funcion; a puede ser "q", "p" o "r" como str no usar char
+    std::cout << std::endl << "=== Modificada ===" << std::endl; //Imprimo lo modificado post idempotencia
+    for(int i=0;i<argList.size();i++){
+        std::cout<<argList[i]<<" Posicion: "<<i<<std::endl;
+    }
     system("Pause");
     return EXIT_SUCCESS;
 }
 
 void participantes() {
     std::cout << std::endl << "=== Taller 01 ===" << std::endl;
-    std::cout << std::endl << "Braulio Argandoña"; // Reemplazar por su nombre
-    std::cout << std::endl << "Fanny Rivero"; // Reemplazar por su nombre
-    std::cout << std::endl << "Jennifer Portiño" << std::endl; // Reemplazar por su nombre
+    std::cout << std::endl << "Braulio Argandoña";
+    std::cout << std::endl << "Fanny Rivero";
+    std::cout << std::endl << "Jennifer Portiño" << std::endl;
 }
 
+void Idempotencia(std::vector<std::string> &argList, std::string a){
+    std::vector<std::string> aux;
+    int c;
+    for(int i=0;i<argList.size();i++)   {
+        if (std::string(argList[i]) == a){    //Encuentra a=p,q o r
+            c=i+1;
+            if (std::string(argList[c]) == "||" || std::string(argList[c]) == "&&" ){ //Encuentra && o ||
+                c+=1;
+                if (std::string(argList[c]) == a){              //Encuentra a nuevamente y verifica la estructura de una idempotencia
+                    int j=0;
+                    for(j; j<=i; j++){
+                        aux.push_back(argList[j]);              //Agrega todo lo anterior a la idempotencia y la variable a al aux
+                    }
+                    c+=1;
+                    for(int q=c; q<argList.size(); q++){
+                        aux.push_back(argList[q]);              //Agrega todo lo posterior a la idempotencia al aux
+                    }
+                    argList=aux;                                //Lista se modifica con lo que sirve
+                    aux.clear();                                //Limpia el auxiliar
+                }
+            }
+        }else{
+            if (std::string(argList[i]) == "~"){
+                c=i+1;
+                if (std::string(argList[c]) == a){
+                    c+=1;
+                    if (std::string(argList[c]) == "||" || std::string(argList[c]) == "&&" ){
+                        c+=1;
+                        if (std::string(argList[c]) == "~"){
+                            c+=1;
+                            if (std::string(argList[c]) == a){
+                                int j=0;
+                                for(j; j<=i+1; j++){
+                                    aux.push_back(argList[j]);
+                                }
+                                c+=1;
+                                for(int q=c; q<argList.size(); q++){
+                                    aux.push_back(argList[q]);
+                                }
+                                argList=aux;
+                                aux.clear();
+                                }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
